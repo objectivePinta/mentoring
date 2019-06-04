f4cf0759c55a1e8d0b905b4cce42e5571ca978d4
Andrei Pinta <andrei.pinta@metrosystems.net>
2019-06-04 09:30:01 +0300

    lALALALALA

---
+++ src/com/company/ArrayProblem.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ArrayProblem {
+
+    /*  1.
+       se citeste n de la tastatura
+       sa se afiseze daca este par sau impar
+    */
+    /*  2.
+        se citeste n de la tastatura
+        se citesc n valori de la tastatura (intregi) care sunt puse intr-un vector
+        sa se calculeze suma tuturor valorilor din vector
+
+        3. se citeste n de la tastatura
+        se citesc n valori de la tastatura (intregi) care sunt puse intr-un vector
+        sa se afiseze valoarea maxima si valoare minima din vector, si numarul de valori egale
+        cu ZERO
+
+
+
+        ex. [ 1,2,3,4,5]
+        max = 5
+        min = 1
+        valoriEgaleCuZero = 3
+         ex. [ 1,2,3,0,0,0,4,5]
+             [ 0,1,2,3,4,5,6,7] -> valorile pe care le ia i
+
+             System.out.println("i="+i+" .Valoare de la pozitia "+ i+" este "+v[i]);
+        max = 5
+
+        min = 0
+        valoriEgaleCuZero = 3
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduceti un numar:");
+        int n = scanner.nextInt();
+    }
+
+//    public static boolean isOdd(int n) {
+//        if (...)
+//    }
+}
